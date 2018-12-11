4fb2ccd766081ea17b1c0ab77b0e24441e383678
Derek Lucas <d@derekplucas.com>
2018-12-11 14:31:47 -0500

    Initial setup

---
+++ src/minicrop.js
@@ -0,0 +1,10 @@
+let minicrop = function(id) {
+  let canvas = document.getElementById(id)
+
+  if (canvas == null) {
+    console.log("Couldn't find element.")
+    return
+  }
+
+  console.log(canvas)
+}
